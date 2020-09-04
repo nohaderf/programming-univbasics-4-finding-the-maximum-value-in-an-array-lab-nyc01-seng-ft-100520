6bb7203fc35aefd49c227fb38be200cc376cb556
Freda Hon <freda.hon@gmail.com>
2020-09-04 16:37:24 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,11 @@
 def find_max_value(array)
-  new_array = 
+  counter = 0
+  n = -1
+  while counter < array.length do
+    if n < array[counter]
+      n = array[counter]
+    end
+    counter += 1
+  end
+   n
 end
